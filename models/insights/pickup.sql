0562caac3e131980ab85638da4a9e5d00cbc0c61
San <sanyam.goel@matillion.com>
2024-01-26 16:04:13 +0000

    change

--- models/insights/pickup.sql
+++ models/insights/pickup.sql
@@ -1,5 +1,5 @@
 SELECT "TaxiZone_pickup", COUNT("TaxiZone_pickup") AS Common_Pickup
-FROM {{ ref('stg_yellow_taxi')}}
+FROM {{ ref('yellow_taxi')}}
 GROUP BY "TaxiZone_pickup"
 ORDER BY Common_Pickup DESC
 LIMIT 1
--- models/insights/vendor.sql
+++ models/insights/vendor.sql
@@ -1,5 +1,5 @@
 SELECT "VendorId", COUNT("VendorId") as No_of_trips
-FROM {{ ref('stg_yellow_taxi')}}
+FROM {{ ref('yellow_taxi')}}
 GROUP BY "VendorId"
 ORDER BY No_of_trips DESC
 LIMIT 1
